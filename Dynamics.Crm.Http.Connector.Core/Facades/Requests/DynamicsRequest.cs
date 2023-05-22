e401116492a0636604133732ad59e03eb5a54ce7
Emmanuel Toledo <emmanueltoledo1082@gmail.com>
2023-05-22 17:23:57 -0600

    Updated Dynamics Request Service.

--- Dynamics.Crm.Http.Connector.Core/Facades/Requests/DynamicsRequest.cs
+++ Dynamics.Crm.Http.Connector.Core/Facades/Requests/DynamicsRequest.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace  Dynamics.Crm.Http.Connector.Core.Facades.Requests
+﻿namespace  Dynamics.Crm.Http.Connector.Core.Facades.Requests
 {
-    internal class DynamicsRequest
+    /// <summary>
+    /// This interface defines the method to call each request to Dynamics.
+    /// </summary>
+    public interface IDynamicsRequest
     {
+        /// <summary>
+        /// Method to send a request to SharePoint
+        /// </summary>
+        /// <param name="request">Http request message configuration.</param>
+        /// <returns>Http response message.</returns>
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
+    }
+
+    /// <summary>
+    /// This class implements the method to call each request to Dynamics.
+    /// </summary>
+    public class DynamicsRequest : IDynamicsRequest
+    {
+        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
